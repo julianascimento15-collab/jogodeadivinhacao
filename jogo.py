2065236137c13695682faf4058a82a47c4ef1727
julianascimento15-collab <julia.nascimento15@escola.pr.gov.br>
2026-06-18 15:13:58 +0000

    salvar

--- jogo.py
+++ jogo.py
@@ -9,9 +9,14 @@ print("você digitou: ",chute)
 
 chuteNumerico = int (chute)
 
+acertou = chuteNumerico == numerosecreto
+maior = chuteNumerico > numerosecreto
+menor = chuteNumerico < numerosecreto
+
+
 #se voce digitar qualquer numero vou verificar se acertou ou errou
 if(numerosecreto==chuteNumerico):
-    print("Você acertou!"
+    print("Você acertou!")
 else:
      print("Você errou!")
 
